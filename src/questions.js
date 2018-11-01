628dd915291ba78b2be5e3817138ddec4b44ffa8
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-11-01 18:54:46 +0100

    created new file to hold the questions

---
+++ src/questions.js
@@ -0,0 +1,144 @@
+const questions = [
+  {
+    id: "200",
+    subject: "chemistry",
+    examtype: "utme",
+    examyear: "2005",
+    question: "Natural water collected from rivers and ponds contains oxygen, carbon(IV) oxide and",
+    option: {
+      a: "Chlorine",
+      b: "Hydrogen",
+      c: "Sulphur(IV) oxide",
+      d: "Nitrogen"
+      },
+    answer: "c"
+  },
+  {
+    id: "107",
+    subject: "chemistry",
+    examtype: "utme",
+    examyear: "2003",
+    question: "Which of the following statements is true of sulphur (IV) oxide",
+    option: {
+      a: "It forms Tetraoxosulphate (VI) acid with water",
+      b: "It is an odourless gas",
+      c: "It is an acid anhydride",
+      d: "It forms white precipitate with acidified barium chloride"
+      },
+    answer: "c"
+  },
+  {
+    id: "271",
+    subject: "chemistry",
+    examtype: "utme",
+    examyear: "2010",
+    question: "Permanent hardness of water can be removed by",
+    option: {
+      a: "Adding slaked lime",
+      b: "Adding caustic soda",
+      c: "Boiling",
+      d: "Filtration"
+    },
+    answer: "b"
+  },
+  {
+    id: "52",
+    subject: "chemistry",
+    examtype: "utme",
+    examyear: "2002",
+    question: "The substance least considered as a source of environmental pollution is",
+    option: {
+      a: "Silicate minerals",
+      b: "Uranium",
+      c: "Lead compounds",
+      d: "Organophosphorous compounds"
+    },
+    answer: "a"
+  },
+  {
+    id: "212",
+    subject: "chemistry",
+    examtype: "utme",
+    examyear: "2005",
+    question: "What will happen if an aluminum spoon is used to stir a solution of iron (II) trioxonitrate(V)",
+    option: {
+      a: "The spoon will be reduced while the iron(II) ions will be oxidized",
+      b: "The spoon will be oxidized while the iron (II) ions will be reduced to the metal",
+      c: "The spoon will be reduced",
+      d: "The spoon will increase in size"
+    },
+    answer: "b"
+  },
+  {
+    id: "123",
+    subject: "physics",
+    examtype: "utme",
+    examyear: "2010",
+    question: "The most likely measurement of length of an object using a venier caliper is: ",
+    option: {
+      a: "3.0cm ",
+      b: "3.3cm ",
+      c: "3.33cm ",
+      d: "3.333cm"
+    },
+    answer: "b"
+  },
+  {
+    id: "134",
+    subject: "physics",
+    examtype: "utme",
+    examyear: "2010",
+    question: "An automobile is traveling at 60km/hr. calculate the angular velocity of the 0.35m radius wheels. ",
+    option: {
+      a: "16.67rad/s ",
+      b: "47.6 rad/s ",
+      c: "21 rad/s ",
+      d: "171.4 rad/s"
+    },
+    answer: "b"
+  },
+  {
+    id: "64",
+    subject: "physics",
+    examtype: "utme",
+    examyear: "2009",
+    question: "A train has an initial velocity of 44m/s and an acceleration of -4m/s2 what is its velocity after 10seconds? ",
+    option: {
+      a: "2m/s ",
+      b: "4m/s ",
+      c: "8m/s ",
+      d: "12m/s "
+    },
+    answer: "b"
+  },
+  {
+    id: "122",
+    subject: "physics",
+    examtype: "utme",
+    examyear: "2012",
+    question: "A 25N force pulls a 2.0kg body up a 300 inclined plane. If force is parallel to the plane and the body moves up the plane at constant velocity. Calculate the magnitude of the frictional force between the body and the plane. [g =10m/s2]",
+    option: {
+      a: "25N",
+      b: "10N",
+      c: "5N",
+      d: "15N"
+    },
+    answer: "d"
+  },
+  {
+    id: "32",
+    subject: "physics",
+    examtype: "utme",
+    examyear: "2007",
+    question: "The temperature at which the water vapour in the air saturates the air and begins to condense is",
+    option: {
+      a: "melting point ",
+      b: "triple point ",
+      c: "dew point ",
+      d: "melting point"
+    },
+    answer: "c"
+  }
+]
+
+export default questions;
