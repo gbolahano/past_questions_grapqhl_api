0a42781dfd32fb3af3d8c01863caf0dff8d664c2
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-11-01 11:44:36 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello World"
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server started at ${url}`);
+});
